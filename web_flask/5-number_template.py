14a21ce42880404b336ee69ea495401edf7fb45d
rajy <kiddo.rajy@gmail.com>
2023-10-22 08:45:53 -0700

    Tasks Completed

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/(<text>): Displays 'Python' followed by the value of <text>.
-    /number/<n>: Displays 'n is a number' only if <n> is an integer.
-    /number_template/<n>: Displays an HTML page only if <n> is an integer.
-"""
+"""Starts a Flask web Application"""
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes=False
 
-
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def c(text):
     """Displays 'C' followed by the value of <text>
 
@@ -38,8 +28,8 @@ def c(text):
     return "C {}".format(text)
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python")
+@app.route("/python/<text>")
 def python(text="is cool"):
     """Displays 'Python' followed by the value of <text>
 
@@ -49,17 +39,18 @@ def python(text="is cool"):
     return "Python {}".format(text)
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route("/number/<int:n>")
 def number(n):
     """Displays 'n is a number' only if <n> is an integer."""
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/<int:n>")
 def number_template(n):
     """Displays an HTML page only if <n> is an integer."""
     return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True)
+    app.run(host="0.0.0.0:5000")
